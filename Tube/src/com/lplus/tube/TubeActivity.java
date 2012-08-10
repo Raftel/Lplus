8b1649d54b9c4fac4eabac7c2cae9e5e84b3059e
Gooson Bazzinga <dwizard@naver.com>
2012-08-11 02:20:02 +0900

    소스 위치 변경 및 폴더 정리, Tube app 추가
    Tube app은 구손이 Facebook에 등록 해놓은 Test용 App 이름

---
+++ Tube/src/com/lplus/tube/TubeActivity.java
@@ -0,0 +1,76 @@
+package com.lplus.tube;
+
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+
+public class TubeActivity extends Activity {
+
+	public static final String APPID = "338804906187760";
+	String[] permissions = new String[] {
+			// User and Friends Permissions
+			  "user_about_me"
+			, "user_activities"
+			, "user_birthday"
+			, "user_checkins"
+			, "user_education_history"
+			, "user_events"
+			, "user_groups"
+			, "user_hometown"
+			, "user_interests"
+			, "user_likes"
+			, "user_location"
+			, "user_notes"
+			, "user_photos"
+			, "user_questions"
+			, "user_relationships"
+			, "user_relationship_details"
+			, "user_religion_politics"
+			, "user_status"
+			, "user_subscriptions"
+			, "user_videos"
+			, "user_website"
+			, "user_work_history"
+			, "email"
+			// Extended Permissions
+			, "read_friendlists"
+			, "read_insights"
+			, "read_mailbox"
+			, "read_requests"
+			, "read_stream"
+			, "xmpp_login"
+			, "ads_management"
+			, "create_event"
+			, "manage_friendlists"
+			, "manage_notifications"
+			, "user_online_presence"
+			, "friends_online_presence"
+			, "publish_checkins"
+			, "publish_stream"
+			, "rsvp_event"
+			// Open Graph Permissions
+			/*, "publish_actions"
+			, "user_actions.music"
+			, "user_actions.news"
+			, "user_actions.video"
+			, "user_actions:APP_NAMESPACE"
+			, "user_games_activity"*/
+			// Page Permissions
+			, "manage_pages"
+		};
+	
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tube);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_tube, menu);
+        return true;
+    }
+
+    
+}
