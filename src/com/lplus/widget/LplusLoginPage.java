d9df2ec76842b3ae67600c34f11d96b65f314f66
Gooson Bazzinga <dwizard@naver.com>
2012-08-11 02:10:08 +0900

    Lplus Widget 추가
    stub 추가

---
+++ src/com/lplus/widget/LplusLoginPage.java
@@ -0,0 +1,24 @@
+package com.lplus.widget;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.ImageView;
+
+public class LplusLoginPage extends ImageView implements LplusPage {
+
+	public LplusLoginPage(Context context, AttributeSet attrs, int defStyle) {
+		super(context, attrs, defStyle);
+		// TODO Auto-generated constructor stub
+	}
+
+	public LplusLoginPage(Context context, AttributeSet attrs) {
+		super(context, attrs);
+		// TODO Auto-generated constructor stub
+	}
+
+	public LplusLoginPage(Context context) {
+		super(context);
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ src/com/lplus/widget/LplusWidget.java
@@ -0,0 +1,5 @@
+package com.lplus.widget;
+
+public interface LplusWidget {
+
+}
