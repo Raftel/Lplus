f7c140843e37b42765b3450df78415b24a44c896
Gooson Bazzinga <dwizard@naver.com>
2012-08-20 16:45:21 +0900

    LplusUtil에 getMin,Max 추가

--- LplusFramework/src/com/lplus/common/LplusUtil.java
+++ LplusFramework/src/com/lplus/common/LplusUtil.java
@@ -8,4 +8,20 @@ public class LplusUtil {
 	public static int dpToPx(DisplayMetrics dm, int dip) {
 		return (int)TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dip, dm);
 	}
+	
+	public static int getMin(int first, int second) {
+		if (first > second) {
+			return second;
+		} else {
+			return first;
+		}
+	}
+	
+	public static int getMax(int first, int second) {
+		if (first < second) {
+			return second;
+		} else {
+			return first;
+		}
+	}
 }
