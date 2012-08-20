5fe5b20456be90221bee6c6081b2e9bed342f1ff
Gooson Bazzinga <dwizard@naver.com>
2012-08-20 15:51:05 +0900

    LplusButtonGruop 추가

---
+++ LplusFramework/src/com/lplus/widget/LplusButtonGroup.java
@@ -0,0 +1,19 @@
+package com.lplus.widget;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.TableLayout;
+
+public class LplusButtonGroup extends TableLayout implements LplusWidget {
+
+	public LplusButtonGroup(Context context, AttributeSet attrs) {
+		super(context, attrs);
+		// TODO Auto-generated constructor stub
+	}
+
+	public LplusButtonGroup(Context context) {
+		super(context);
+		// TODO Auto-generated constructor stub
+	}
+
+}
