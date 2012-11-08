e2ce20152ca8dfdcf56c7f80a02a0252d9e92b96
Gooson Bazzinga <dwizard@naver.com>
2012-11-09 01:58:18 +0900

    Swing Animation 추가

--- LplusFramework/src/com/lplus/animation/LplusAnimationUtil.java
+++ LplusFramework/src/com/lplus/animation/LplusAnimationUtil.java
@@ -1,7 +1,5 @@
 package com.lplus.animation;
 
-import com.lplus.widget.LplusLoginButton;
-
 import android.view.View;
 
 public class LplusAnimationUtil {
@@ -10,5 +8,16 @@ public class LplusAnimationUtil {
 		LplusAnimation anim = LplusAnimationManager.getInstance().createAnimation(0, 500, LplusAnimation.FUNC_EASE_OUT);
         anim.addProperty(new LplusAnimationProp(LplusAnimationProp.PROP_ROTATE_Y, view, 0.0f, 360.0f));
         anim.start();
+	}
+	
+	public static void swing(View view) {
+		LplusAnimation animRight = LplusAnimationManager.getInstance().createAnimation(0, 1000, LplusAnimation.FUNC_EASE_OUT);
+		animRight.addProperty(new LplusAnimationProp(LplusAnimationProp.PROP_TRANSLATE_X, view, view.getTranslationX(), view.getRootView().getWidth()));
+        
+		LplusAnimation animLeft = LplusAnimationManager.getInstance().createAnimation(500, 500, LplusAnimation.FUNC_EASE_OUT);
+        animLeft.addProperty(new LplusAnimationProp(LplusAnimationProp.PROP_TRANSLATE_X, view, view.getRootView().getWidth(), -view.getRootView().getWidth()));
+        
+        animRight.addNextAnimation(animLeft);
+        animRight.start();
 	}
 }
